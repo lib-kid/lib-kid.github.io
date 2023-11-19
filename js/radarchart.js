58e57aeacd90fbd34f8af1560e0075aeb7bc0d1d
lib-kid <146098871+lib-kid@users.noreply.github.com>
2023-11-20 01:51:05 +0900

    Add files

--- js/radarchart.js
+++ js/radarchart.js
@@ -1,61 +1,81 @@
-var ctx = document.getElementById("myChart1");
-var myRadarChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: ["学習目標", "評価方法", "教授方略", "教材改善"],
-        datasets: [{
-            label: '結果',
-            data: [0, 1, 2, 4],
-            borderColor: 'rgb(54, 162, 235)',
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderWidth: 1,
-        }],
-    },
-    options: {
-        scales: {
-            r: {
-                pointLabels: {
-                    font: {
-                        size: 16,
+var myRadarChart1
+
+document.querySelector('#btn1').addEventListener('click', () => {
+    if (myRadarChart1) myRadarChart1.destroy();
+    var ctx = document.getElementById("myChart1");
+    check1_1 = document.querySelectorAll("input[name=goal]:checked").length;
+    check1_2 = document.querySelectorAll("input[name=test]:checked").length;
+    check1_3 = document.querySelectorAll("input[name=instruction]:checked").length;
+    check1_4 = document.querySelectorAll("input[name=improvement]:checked").length;
+
+    myRadarChart1 = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ["学習目標", "評価方法", "教授方略", "教材改善"],
+            datasets: [{
+                label: '結果',
+                data: [check1_1, check1_2, check1_3, check1_4],
+                borderColor: 'rgb(54, 162, 235)',
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                borderWidth: 1,
+            }],
+        },
+        options: {
+            scales: {
+                r: {
+                    pointLabels: {
+                        font: {
+                            size: 16,
+                        },
                     },
+                    min: 0,
+                    max: 4,
+                    ticks: {
+                        stepSize: 1,
+                    }
                 },
-                min: 0,
-                max: 4,
-                ticks: {
-                    stepSize: 1,
-                }
             },
         },
-    },
+    });
 });
 
-var ctx = document.getElementById("myChart2");
-var myRadarChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: ["注意", "関連性", "自信", "満足感"],
-        datasets: [{
-            label: '結果',
-            data: [0, 1, 2, 3],
-            borderColor: 'rgb(54, 162, 235)',
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderWidth: 1,
-        }],
-    },
-    options: {
-        scales: {
-            r: {
-                pointLabels: {
-                    font: {
-                        size: 16,
+var myRadarChart2
+
+document.querySelector('#btn2').addEventListener('click', () => {
+    if (myRadarChart2) myRadarChart2.destroy();
+    var ctx = document.getElementById("myChart2");
+    check2_1 = document.querySelectorAll("input[name=attention]:checked").length;
+    check2_2 = document.querySelectorAll("input[name=relevance]:checked").length;
+    check2_3 = document.querySelectorAll("input[name=confidence]:checked").length;
+    check2_4 = document.querySelectorAll("input[name=satisfaction]:checked").length;
+
+    myRadarChart2 = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ["注意", "関連性", "自信", "満足感"],
+            datasets: [{
+                label: '結果',
+                data: [check2_1, check2_2, check2_3, check2_4],
+                borderColor: 'rgb(54, 162, 235)',
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                borderWidth: 1,
+            }],
+        },
+        options: {
+            scales: {
+                r: {
+                    pointLabels: {
+                        font: {
+                            size: 16,
+                        },
                     },
+                    min: 0,
+                    max: 3,
+                    ticks: {
+                        stepSize: 1,
+                    }
                 },
-                min: 0,
-                max: 3,
-                ticks: {
-                    stepSize: 1,
-                }
             },
         },
-    },
+    });
 });
